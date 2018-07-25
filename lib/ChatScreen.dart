b1e151519522e961d91bb0d924caddae3dca54db
Abel Fernandez <afern@thoughtworks.com>
2018-07-25 09:29:14 +0200

    add visual improvements base in the platform (ios-android)

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -42,7 +42,7 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
         ),
         decoration: Theme.of(context).platform == TargetPlatform.iOS ?
         BoxDecoration(
-          border: Border(IT
+          border: Border(
             top: BorderSide(color: Colors.grey[200])
           )
         ) : null,
